6871508452f5f39793878fdae66f1f2c4606b660
Ferdinand Teguefouet <teguefouet1@gmail.com>
2021-12-16 16:00:34 +0100

    J'ai terminé avec l'affichage des films. Lorsqu'on clique sur un film, le detail de celui-ci est affiché dans une bouvelle page.

--- src/app/movie/models/movie.model.ts
+++ src/app/movie/models/movie.model.ts
@@ -1,7 +1,6 @@
 export interface Movie{
     id: number;
     original_title: string;
-    overview: string;
     poster_path: string;
     page : number;
     total_pages: number;
@@ -10,32 +9,17 @@ export interface Movie{
 
 export interface MovieDetail{
     backdrop_path: string
-    belongs_to_collection:string
-    buget: number
-    genres: Genres[]
+    budget: number
+    genres: any[]
     homepage: string
     original_language: string
     original_title: string
-    overviex: string
+    overview: string
     popularity: number
-    production_companies: ProductionCompanies[]
-    Production_countries: ProductionCountries[]
+    production_companies: any[]
+    production_countries: any[]
     release_date: string
     status: string
     vote_average: number
     vote_count: number
-}
-
-export interface Genres{
-    name: string
-}
-
-export interface ProductionCompanies{
-    logo_path: string
-    name : string
-    origin_country: string
-}
-
-export interface ProductionCountries{
-    name: string
 }
--- src/app/movie/movie-detail/movie-detail.component.ts
+++ src/app/movie/movie-detail/movie-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
+import { MovieDetail } from '../models/movie.model';
 
 @Component({
   selector: 'app-movie-detail',
@@ -7,13 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailComponent implements OnInit {
 
-  constructor() { }
+  oneMovie!: MovieDetail
+  img! : string
+  id! : number
+
+  constructor(private readonly _router: Router,
+              private readonly _mService: MovieService,
+              private readonly _activetedroute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.img = this._mService.url_img
+    this.id = this._activetedroute.snapshot.params.id
+    //console.log(this.id)
+
+    this.getOneMovie(this._mService.url_api+"/movie/"+String(this.id)+"?api_key=62f623f39673f5defe37553f5d64bddc&language=fr-FR")
   }
 
   getOneMovie(url: string){
-
+    this._mService.GetById(url).subscribe(
+      (OneMovieFormService: MovieDetail) => {
+      this.oneMovie = OneMovieFormService
+      console.log(this.oneMovie)
+      })
   }
 
 }
